64c9c82fea9692dcbb17fd82a7e5e9d1a7cd71c0
anhahyoun <dksgkgus@gmail.com>
2024-03-31 20:17:23 +0900

    [feature] 인원 필터 카드 생성

--- feature/src/main/java/com/grabsome/feature/home/filter/HomeFilterCard.kt
+++ feature/src/main/java/com/grabsome/feature/home/filter/HomeFilterCard.kt
@@ -1,4 +1,4 @@
-@file:OptIn(ExperimentalLayoutApi::class)
+@file:OptIn(ExperimentalLayoutApi::class, ExperimentalLayoutApi::class)
 
 package com.grabsome.feature.home.filter
 
@@ -6,15 +6,25 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.grabsome.core.designsystem.component.chips.Chips
 import com.grabsome.core.designsystem.component.chips.ChipsType
+import com.grabsome.core.designsystem.component.controls.CheckBox
+import com.grabsome.core.designsystem.component.controls.CheckBoxSize
+import com.grabsome.core.designsystem.component.controls.Slider
 import com.grabsome.core.designsystem.component.inputfield.SearchField
 import com.grabsome.core.designsystem.theme.color.color
 import com.grabsome.core.designsystem.theme.typography.typography
@@ -72,6 +82,11 @@ fun HomeRegionFilterCard() {
 
 @Composable
 fun HomeCountFilterCard() {
+    // todo 네이밍 Participants로??
+    // todo sliderPosition, isUnlimitedParticipants 값 뷰모델로 이동
+    var sliderPosition by remember { mutableStateOf(1f..10f) }
+    var isUnlimitedParticipants by rememberSaveable { mutableStateOf(true) }
+
     Column(
         modifier = Modifier
             .padding(16.dp)
@@ -83,6 +98,64 @@ fun HomeCountFilterCard() {
             color = color.neutral900,
             modifier = Modifier.fillMaxWidth(),
         )
+        CheckBox(
+            modifier = Modifier.padding(vertical = 16.dp),
+            checked = isUnlimitedParticipants,
+            label = "인원 무관",
+            checkBoxSize = CheckBoxSize.MEDIUM
+        ) {
+            isUnlimitedParticipants = isUnlimitedParticipants.not()
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "선호하는 인원",
+                style = typography.titleXSmall,
+                color = color.neutral600,
+            )
+            Text(
+                text = if (isUnlimitedParticipants) "무관" else "${sliderPosition.start.toInt()} ~ ${sliderPosition.endInclusive.toInt()}명",
+                style = typography.titleMedium,
+                color = color.blue300,
+            )
+        }
+
+        Slider(
+            modifier = Modifier.fillMaxWidth(),
+            value = sliderPosition,
+            onValueChange = { range -> sliderPosition = range },
+            steps = 8,
+            valueRange = 1f..10f,
+            enabled = !isUnlimitedParticipants
+        )
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                modifier = Modifier.padding(horizontal = 12.dp),
+                text = "1명",
+                style = typography.bodySmall,
+                color = color.neutral400
+            )
+            Text(
+                modifier = Modifier.padding(horizontal = 12.dp),
+                text = "5명",
+                style = typography.bodySmall,
+                color = color.neutral400
+            )
+            Text(
+                modifier = Modifier.padding(horizontal = 12.dp),
+                text = "10명",
+                style = typography.bodySmall,
+                color = color.neutral400
+            )
+        }
     }
 }
 
